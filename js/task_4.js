7640a7f3d126811dd9fbbeb1c1dcfb07ead146e6
Daryna <66668459+DarynaProkopchuk@users.noreply.github.com>
2020-10-09 16:32:51 +0400

    task4 done

---
+++ js/task_4.js
@@ -0,0 +1,32 @@
+const countTotalSalary = function(employees) {
+    'use strict';
+    // Write code under this line
+   
+        let TotalSalary = 0;
+    
+        for (const employee in employees) {
+          
+            TotalSalary += employees[employee];
+           }
+        
+        return TotalSalary;
+     
+  };
+  
+  // Объекты и ожидаемый результат
+  const developers = {
+      mango: 300,
+      poly: 250,
+      alfred: 450,
+  };
+  console.log(countTotalSalary(developers));
+  // 1000
+  
+  const supports = {
+    kiwi: 200,
+    lux: 150,
+    chelsy: 150,
+  }
+  console.log(countTotalSalary(supports));
+  // 500
+  
